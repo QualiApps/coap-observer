cd84a57f0c53cdd7d5ca688e2fbd58621af15e05
Yury Kavaliou <yury_kavaliou@epam.com>
2015-12-01 16:00:54 +0300

    ES driver

---
+++ db/es/es.go
@@ -0,0 +1,166 @@
+package es
+
+import (
+	"gopkg.in/olivere/elastic.v2"
+	"log"
+	"net/url"
+	"strings"
+)
+
+var (
+	DIndex = "index" //default index name
+	DType  = "type"  // default type name
+)
+
+type ES struct {
+	Conn  *elastic.Client
+	Host  string
+	Port  string
+	Index string
+	Type  string
+}
+
+func NewES(host, port string) (*ES, error) {
+	es := &ES{Host: host, Port: port}
+
+	url := es.GetUrl(host, port)
+
+	var err error
+	es.Conn, err = elastic.NewClient(
+		elastic.SetURL(url),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	_, _, err = es.Conn.Ping().Do()
+	if err != nil {
+		return nil, err
+	}
+
+	return es, nil
+}
+
+func (e *ES) GetUrl(h, p string) string {
+	u := &url.URL{
+		Scheme: "http",
+		Host:   strings.Join([]string{h, p}, ":"),
+	}
+
+	return u.String()
+}
+
+func (e *ES) GetType() string {
+	if e.isEmpty(e.Type) {
+		e.Type = DType
+	}
+	return e.Type
+}
+
+func (e *ES) SetType(t string) {
+	e.Type = t
+}
+
+func (e *ES) GetIndex() string {
+	if e.isEmpty(e.Index) {
+		e.Index = DIndex
+	}
+	return e.Index
+}
+
+func (e *ES) SetIndex(i string) {
+	e.Index = i
+}
+
+func (e *ES) Get(id string) []byte {
+	var data = []byte("")
+	// Get item with specified ID
+	get, err := e.Conn.Get().
+		Index(e.GetIndex()).
+		Type(e.GetType()).
+		Id(id).
+		Do()
+	if err != nil {
+		log.Printf("GET ERROR! %#v", err)
+	}
+	if get.Found {
+		data = []byte(*get.Source)
+	}
+
+	return data
+}
+
+func (e *ES) Post(data, id string) (string, bool) {
+	return e.send(data)
+}
+
+func (e *ES) Put(data, id string) (string, bool) {
+	return e.send(data)
+}
+
+/**
+ * @TODO - needs to implement
+ */
+func (e ES) Delete(id string) bool {
+	return false
+}
+
+func (e *ES) CreateIndex(i string) error {
+	return e.checkIndex(i)
+}
+
+func (e *ES) DeleteIndex(i string) bool {
+	isDelete := true
+	_, err := e.Conn.DeleteIndex(i).Do()
+	if err != nil {
+		log.Printf("DELETE INDEX ERROR: %#v\n", err)
+		isDelete = false
+	}
+	return isDelete
+}
+
+func (e *ES) checkIndex(i string) error {
+	// Use the IndexExists service to check if a specified index exists.
+	exist, err := e.Conn.IndexExists(i).Do()
+	if err != nil {
+		// Set error
+		return err
+	}
+	if !exist {
+		// Create a new index.
+		createIndex, err := e.Conn.CreateIndex(i).Do()
+		if err != nil {
+			// set error
+			return err
+		}
+		if !createIndex.Acknowledged {
+			// Not acknowledged
+		}
+	}
+	e.SetIndex(i)
+
+	return nil
+}
+
+func (e *ES) send(data string) (string, bool) {
+	res, err := e.Conn.Index().
+		Index(e.GetIndex()).
+		Type(e.GetType()).
+		BodyString(data).
+		Do()
+	if err != nil {
+		log.Printf("POST ERROR: %#v\n", err)
+		return "", false
+	}
+
+	return res.Id, true
+}
+
+func (e *ES) isEmpty(s string) bool {
+	empty := false
+	if len(strings.TrimSpace(s)) == 0 {
+		empty = true
+	}
+
+	return empty
+}
---
+++ db/es/proc.go
@@ -0,0 +1,32 @@
+package es
+
+import (
+	"encoding/json"
+	"log"
+)
+
+const dbTypeKey = "type"
+
+var mmap map[string]interface{}
+
+func (e *ES) Processing(payload string) {
+	json.Unmarshal([]byte(payload), &mmap)
+	// Send data if decode ok (json format)
+	if mmap != nil {
+		// set default type
+		e.SetType(DType)
+		// init a particular type
+		if val, ok := mmap[dbTypeKey]; ok {
+			switch eventType := val.(type) {
+			case string:
+				e.SetType(eventType)
+			}
+
+		}
+		// post to db
+		if _, ok := e.Post(payload, ""); !ok {
+			log.Printf("Can't add item %s to DB!\n", payload)
+		}
+	}
+
+}
